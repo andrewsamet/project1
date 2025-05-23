4f123a0d33013dc8551f60f92157dab9b31f4650
Andrew James Samet <a.j.samet@se23.qmul.ac.uk>
2025-05-23 14:19:32 +0100

    test

---
+++ Test.py
@@ -0,0 +1,17 @@
+import requests
+# url = f"https://api.football-data.org/v4/competitions/PL/teams"
+# headers = {"X-Auth-Token": "2e37a6b44f0c47f18526dec884897e45"}
+# response = requests.get(url, headers=headers)
+#
+# teams = response.json()['teams']
+# for i in teams:
+#     if i['name'] == "Arsenal FC":
+#         print(i['name'])
+
+url = f"https://api.football-data.org/v4/competitions/PL/standings"
+headers = {"X-Auth-Token": "2e37a6b44f0c47f18526dec884897e45"}
+response = requests.get(url, headers=headers)
+
+table = response.json()['standings'][0]['table']
+for j,i in enumerate(table, start=1):
+    print(f"{j}. {i['team']['name']}")
--- main.py
+++ main.py
@@ -1,89 +1,67 @@
 import requests
-# """requests allows us to access data online. headers means any extra information thats needed for the url,
-# e.g. our AIP key"""
-# url = f"https://api.football-data.org/v4/teams/66/matches"
-# headers = {"X-Auth-Token": "2e37a6b44f0c47f18526dec884897e45"}
-# response = requests.get(url, headers = headers)
-# """We iterate through the lists of matches, and select home team of each match and the name, like
-# a dictionary"""
-# matches = response.json()['matches']
-# for i in matches:
-#     hometeam = i['homeTeam']['name']
-#     awayteam = i['awayTeam']['name']
-#     print(f"{hometeam} vs {awayteam}")
-premier_league_teams = {
-    "Arsenal": 57,
-    "Aston Villa": 58,
-    "Bournemouth": 1044,
-    "Brentford": 402,
-    "Brighton": 397,
-    "Burnley": 328,
-    "Chelsea": 61,
-    "Crystal Palace": 354,
-    "Everton": 62,
-    "Fulham": 63,
-    "Liverpool": 64,
-    "Luton Town": 389,
-    "Manchester City": 65,
-    "Manchester United": 66,
-    "Newcastle United": 67,
-    "Nottingham Forest": 351,
-    "Sheffield United": 356,
-    "Tottenham Hotspur": 73,
-    "West Ham United": 563,
-    "Wolverhampton Wanderers": 76
-}
 
 class Team():
-    premier_league_teams = {
-        "Arsenal": 57,
-        "Aston Villa": 58,
-        "Bournemouth": 1044,
-        "Brentford": 402,
-        "Brighton": 397,
-        "Burnley": 328,
-        "Chelsea": 61,
-        "Crystal Palace": 354,
-        "Everton": 62,
-        "Fulham": 63,
-        "Liverpool": 64,
-        "Luton Town": 389,
-        "Manchester City": 65,
-        "Manchester United": 66,
-        "Newcastle United": 67,
-        "Nottingham Forest": 351,
-        "Sheffield United": 356,
-        "Tottenham Hotspur": 73,
-        "West Ham United": 563,
-        "Wolverhampton Wanderers": 76
-    }
-
     def __init__(self, name):
-        if name in Team.premier_league_teams:
-            self.name = name
-            self.team_id = self.premier_league_teams[name]
-            self.fixtures = []
-            url = f"https://api.football-data.org/v4/teams/{self.team_id}/matches"
-            headers = {"X-Auth-Token" : "2e37a6b44f0c47f18526dec884897e45"}
-            response = requests.get(url, headers = headers)
-
-            for i in response.json()['matches']:
-                hometeam = i['homeTeam']['name']
-                awayteam = i['awayTeam']['name']
-                self.fixtures.append(f"{hometeam} vs {awayteam}")
-        else:
-            raise ValueError(f"{name} is not a valid premier league team, sorry.")
+        self.name = name
+        url = f"https://api.football-data.org/v4/competitions/PL/teams"
+        headers = {"X-Auth-Token": "2e37a6b44f0c47f18526dec884897e45"}
+        response = requests.get(url, headers=headers)
+        teams = response.json()['teams']
+        for team in teams:
+            if team['name'] == name:
+                self.team_id = team['id']
+                break
+            else:
+                raise ValueError(f"{name} is not a valid premier league team, sorry.")
+        self.fixtures = []
+        url = f"https://api.football-data.org/v4/teams/{self.team_id}/matches"
+        headers = {"X-Auth-Token": "2e37a6b44f0c47f18526dec884897e45"}
+        response = requests.get(url, headers=headers)
+        for i in response.json()['matches']:
+            hometeam = i['homeTeam']['name']
+            awayteam = i['awayTeam']['name']
+            self.fixtures.append(f"{hometeam} vs {awayteam}")
 
     def show_fixtures(self):
         for i in self.fixtures:
             print(i)
         return
 
-# ManUtd = Team(66, 'Man Utd')
-# ManUtd.show_fixtures()
-Chosen_team = input("What team would you like to explore?")
-team = Team(Chosen_team)
-team.show_fixtures()
+    def see_table(self):
+        url = f"https://api.football-data.org/v4/competitions/PL/standings"
+        headers = {"X-Auth-Token": "2e37a6b44f0c47f18526dec884897e45"}
+        response = requests.get(url, headers=headers)
+
+        table = response.json()['standings'][0]['table']
+        for j, i in enumerate(table, start=1):
+            print(f"{j}. {i['team']['name']}")
+
+
+class Run():
+    def what_team(self):
+        name = input("What team would you like to explore? \n")
+        while True:
+            try:
+                team = Team(name)
+                self.what_to_do(team)
+                break
+            except ValueError as e:
+                print(e)
+                break
+
+    def what_to_do(self, team):
+        to_do = input(f"What woud you like to explore about {team.name}? \n")
+        if to_do == "Fixture List":
+            return team.show_fixtures()
+        elif to_do == "See Table":
+            return team.see_table()
+        else:
+            print("Invalid Request\nPlease Try Again")
+            return self.what_to_do(team)
+
+app = Run()
+app.what_team()
+
 
 
 
