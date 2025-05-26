0afef9c539c1d67df290e840a648b1709ce47ae6
Andrew James Samet <a.j.samet@se23.qmul.ac.uk>
2025-05-26 20:14:07 +0100

    footy.1

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import requests
+import os
+from dotenv import load_dotenv
 # from Options import Opt
+load_dotenv()
+api_token = os.getenv("API_TOKEN")
 
 class Team():
     def __init__(self, name):
         self.name = name
         url = f"https://api.football-data.org/v4/competitions/PL/teams"
-        headers = {"X-Auth-Token": "2e37a6b44f0c47f18526dec884897e45"}
+        headers = {"X-Auth-Token": f"{api_token}"}
         response = requests.get(url, headers=headers)
         teams = response.json()['teams']
         found = False
